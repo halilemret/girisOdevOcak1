676182927792f6168db3be4ffe41ec5dee8b692f
halilemre <halilemretopaloglu@gmail.com>
2025-03-16 06:06:23 +0300

    Proje dosyası ekle.

---
+++ girisOdevOcak1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Adınız: ");
+        string ad = Console.ReadLine();
+        Console.WriteLine(ad);
+
+    }
+}
